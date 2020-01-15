6a6095b99f5039ef129c78c799a9601f225239f9
Jonnyy <caijishiwo@gmail.com>
2020-01-14 23:47:02 -0500

    list-actions

---
+++ src/components/actions/action-types/list-actions.js
@@ -0,0 +1,3 @@
+//Types should be in const to avoid typos and duplication since it's a string and could be easily miss spelled
+export const ADD_TO_LIST = 'ADD_TO_LIST' 
+export const REMOVE_ITEM = 'REMOVE_ITEM'
