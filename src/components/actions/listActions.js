704cab334f6d1efaef971c58d87bf99df0bee975
Jonnyy <caijishiwo@gmail.com>
2020-01-14 23:46:52 -0500

    init listAction

---
+++ src/components/actions/listActions.js
@@ -0,0 +1,15 @@
+import {ADD_TO_LIST, REMOVE_ITEM} from './action-types/list-actions'
+//add list action
+export const addToList = (id)=>{
+    return{
+        type: ADD_TO_LIST,
+        id
+    }
+}
+
+export const removeItem = (id)=>{
+    return {
+        type: REMOVE_ITEM,
+        id
+    }
+}
