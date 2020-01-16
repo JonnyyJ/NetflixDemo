20ac525bf55ebe8ca1b1439e52b544e4d9b8b8db
Jonnyy <caijishiwo@gmail.com>
2020-01-16 14:38:35 -0500

    added listReducer

--- src/components/reducers/listReducer.js
+++ src/components/reducers/listReducer.js
@@ -1,7 +1,22 @@
 import {ADD_TO_LIST, REMOVE_ITEM} from '.actions/action-types/list-actions'
-
+import data from './data'
 const initiaState = {
     items: [
+         ...data.mylist.map(s => ({s})),
+         ...data.recommendations.map(s =>({s}))
+    ],
+    addedItems:[],
+    total: 0
+}
+const listReducer = (state = initState, action) =>{
+    //Inside home component
+    if(action.type === ADD_TO_LIST){
+        let addedItem = state.items.find(item => item.id === action.id)
+    }
+    if(action.type === REMOVE_ITEM){
+        let itemToRemove = state.addedItems.find(item =>action.id === item.id),
+            new_items = state.addedItems.filter(item => action.id !== item.id)
         
-    ]
+            //calculation
+    }
 }
