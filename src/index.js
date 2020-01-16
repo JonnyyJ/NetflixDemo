077898fef00d72ffe32b1fdf392cafd1e1f36b49
Jonnyy <caijishiwo@gmail.com>
2020-01-16 14:37:20 -0500

    rewrite the index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App';
+import './index.css'
+import listReducer from './components/reducers/listReducer'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 
-const App = () => (
-    <div>
-        <h1>123</h1>
-    </div>
-)
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+const store = createStore(listReducer, applyMiddleware(thunk))
+ReactDOM.render(<Provider store = {store}><App/></Provider>, document.getElementById('root'))
