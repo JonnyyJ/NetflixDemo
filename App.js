642d3ec81a278328cbd47a7162308e47bb1254f1
Jonnyy <caijishiwo@gmail.com>
2020-01-16 14:37:03 -0500

    added render part

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-const App = () =>{
-    return (
-        <div> <p>123456</p></div>
-    )
+import {BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './components/Home'
+import List from './components/List'
+class App extends Component{
+    render(){
+        return (
+            <BrowserRouter>
+             <div className = "App">
 
+                 <Navbar/>
+                   <Switch>
+                       <Route exact path = "/" component = {Home}/>
+                       <Route path = "/list" component = {List}/>
+                   </Switch>
+             </div>
+            </BrowserRouter>
+        )
+    }
 }
 export default App;
-ReactDOM.render(<App/>, document.getElementById("app"))
