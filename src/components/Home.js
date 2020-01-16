5b94343da08a4c8703f611de93eb83d8b74bfe9b
Jonnyy <caijishiwo@gmail.com>
2020-01-16 14:36:50 -0500

    added render part

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,39 @@ class Home extends Component {
         this.props.addToList(id)
     }
     render(){
-        
+        let itemList = this.props.items.map(item=>{
+            return(
+                <div key = {item.id}>
+                    <div>
+                        <div>{item.title}</div>
+                        <button to ='/' onClick={()=>{this.hanldeClick(item.id)}}><i className ="material-icons">add</i></button>
+                    </div>
+
+                    <div>
+                         <p>{item.desc}</p>
+                         <p><b>Price: {item.price}$</b></p>
+                    </div>
+                </div>
+            )
+        })
+        return (
+            <div className = "container">
+                <h3 className = "center">React-Redux-Store</h3>
+                <div className = "box">
+                    {itemList}
+                </div>
+            </div>
+        )
     }
 }
+const mapStateToProps = (state) =>{
+    return{
+        items: state.items
+    }
+}
+const mapleDispatchToProps = (dispatch) =>{
+    return {
+        addToList: (id) =>{dispatch(addToList(id))}
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
